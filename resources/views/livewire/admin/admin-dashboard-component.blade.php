0854c14bab49489851a776454b4c4173c1eceee3
Javed Nasir Mir <javedmir@gmail.com>
2021-07-03 11:07:34 +0800

    Add Product To Wishlist

--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -1,3 +1,24 @@
 <div>
-    <h1>Admin Dashboard</h1>
+    <div class="container" style="padding: 30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h1>Admin Dashboard</h1>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h1>Admin body</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div>    
 </div>
