5423a94319b9df1789b36df73f9516793192c1b6
Javed Nasir Mir <javedmir@gmail.com>
2021-07-05 15:08:23 +0800

    Commerce - Admin Apply Coupons

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -55,9 +55,16 @@
 					<div class="order-summary">
 						<h4 class="title-box">Order Summary</h4>
 						<p class="summary-info"><span class="title">Subtotal</span><b class="index">${{Cart::instance('cart')->subtotal()}}</b></p>
-						<p class="summary-info"><span class="title">Tax</span><b class="index">${{Cart::instance('cart')->tax()}}</b></p>
-						<p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
-						<p class="summary-info total-info "><span class="title">Total</span><b class="index">${{Cart::instance('cart')->subtotal()}}</b></p>
+						@if(session::has('coupon'))
+							<p class="summary-info"><span class="title">Discount ({{Session::get('coupon')['code']}}) <a href="" wire:click.prevent="removeCoupon"><i class="fa fa-times text-danger"></i></a></span><b class="index"> -${{ $discount}}</b></p>
+							<p class="summary-info"><span class="title">Subtotal with Discount</span><b class="index">${{ number_format($subtotalAfterDiscount, 2)}}</b></p>
+							<p class="summary-info"><span class="title">Tax ({{config('cart.tax')}}%)</span><b class="index">${{ number_format($taxAfterDiscount, 2)}}</b></p>							
+							<p class="summary-info total-info "><span class="title">Total</span><b class="index">${{ number_format($totalAfterDiscount, 2)}}</b></p>							
+						@else
+							<p class="summary-info"><span class="title">Tax</span><b class="index">${{Cart::instance('cart')->tax()}}</b></p>
+							<p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
+							<p class="summary-info total-info "><span class="title">Total</span><b class="index">${{Cart::instance('cart')->total()}}</b></p>
+						@endif						
 					</div>
 
 					<div class="wrap-iten-in-cart">
@@ -95,15 +102,35 @@
 							<p>No item saved for later.</p>
 						@endif
 				</div>
-
-
+					
 					<div class="checkout-info">
-						<label class="checkbox-field">
-							<input class="frm-input " name="have-code" id="have-code" value="" type="checkbox"><span>I have promo code</span>
-						</label>
+						@if(!Session::has('coupon'))							
+							<label class="checkbox-field">
+								<input class="frm-input " name="have-code" id="have-code" value="" type="checkbox" wire:model="haveCouponCode"><span>I have coupon code</span>
+							</label>						
+							@if($haveCouponCode == 1)
+								<div class="summary-items">
+									<form wire:submit.prevent="applyCouponCode">
+										<h4 class="title-box">Coupon Code</h4>
+										@if(Session::has('coupon_message'))
+											<p class="alert alert-danger">{{ Session::get('coupon_message') }}</p>
+										@endif
+										<p class="row-in-form">
+											<label for="coupon-code">Enter your coupon code:</label>
+											<input type="text" name="coupon-code" wire:model="couponCode" />
+										</p>
+										<button type="submit" class="btn btn-small">Apply</button>
+									</form>
+								</div>
+							@endif
+						@endif
+						<br>
+						@if(Session::has('message'))
+							<p class="alert alert-danger">{{ Session::get('message') }}</p>
+						@endif
 						<a class="btn btn-checkout" href="checkout.html">Check out</a>
 						<a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
-					</div>
+					</div>					
 					<div class="update-clear">
 						<a class="btn btn-clear" href="#" wire:click.prevent="destroyAll()">Clear Shopping Cart</a>
 						<a class="btn btn-update" href="#">Update Shopping Cart</a>
