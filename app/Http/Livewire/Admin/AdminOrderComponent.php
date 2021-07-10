3f1ed726c841d5dee88d1e6eca5b1e1d20d29156
Javed Nasir Mir <javedmir@gmail.com>
2021-07-10 15:03:27 +0800

    User Show Orders and Order Details

--- app/Http/Livewire/Admin/AdminOrderComponent.php
+++ app/Http/Livewire/Admin/AdminOrderComponent.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Livewire\Admin;
 
+use Illuminate\Support\Facades\DB;
 use App\Models\Order;
 use Livewire\Component;
 
 class AdminOrderComponent extends Component
 {
+    public function updateOrderStatus($order_id, $status)
+    {
+        $order = Order::find($order_id);
+        $order->status = $status;
+        if($status == 'delivered')
+        {
+            $order->delivered_date = DB::raw('CURRENT_DATE');
+        }
+        else if($status == 'canceled')
+        {
+            $order->delivered_date = DB::raw('CURRENT_DATE');
+        }
+        $order->save();
+        session()->flash('order_message', 'Order status is updated successfully!');
+    }
+
     public function render()
     {
         $orders = Order::orderBy('created_at', 'DESC')->paginate(10);
--- resources/views/livewire/admin/admin-order-details-component.blade.php
+++ resources/views/livewire/admin/admin-order-details-component.blade.php
@@ -1,10 +1,9 @@
 <div>
     <div class="container" style="padding: 30px 0;">
-
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
+                <div class="panel-heading">
                         <div class="row">
                             <div class="col-md-6">
                                 <h4>Order Details</h4>
@@ -12,6 +11,39 @@
                             <div class="col-md-6">
                                 <a href="{{ route('admin.orders') }}" class="btn btn-success pull-right">All Orders</a>
                             </div>
+                            <div class="panle-body">
+                                <table class="table">
+                                    <tr>
+                                        <th>Order Id</th>
+                                        <td>{{ $order->id }}</td>
+                                        <th>Order Date</th>
+                                        <td>{{ $order->created_at }}</td>
+                                        <th>Order Status</th>
+                                        <td>{{ $order->status }}</td>
+                                        @if($order->status == 'delivered')
+                                            <th>Delivery Date</th>
+                                            <td>{{ $order->delivered_date }}</td>
+                                        @elseif($order->status == 'canceled')
+                                            <th>Cancellation Date</th>
+                                            <td>{{ $order->canceled_date }}</td>
+                                        @endif
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h4>Order Details</h4>
+                            </div>                            
                         </div>
                     </div>
                     <div class="panel-body">
